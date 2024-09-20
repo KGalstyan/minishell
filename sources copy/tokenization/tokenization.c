80e9d8dc43736bf3dc2be8882f0805d92b5ac54e
Karen Galstyan <kgalstya@c2r4s3.42yerevan.am>
2024-09-20 15:51:29 +0400

    ...

--- sources copy/tokenization/tokenization.c
+++ sources copy/tokenization/tokenization.c
@@ -6,20 +6,18 @@
 /*   By: kgalstya <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/16 16:31:25 by vkostand          #+#    #+#             */
-/*   Updated: 2024/09/20 15:04:41 by kgalstya         ###   ########.fr       */
+/*   Updated: 2024/09/20 15:49:58 by kgalstya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	ft_strncmp(const char *s1, const char *s2, size_t n)
+int	ft_strcmp(const char *s1, const char *s2)
 {
 	size_t	i;
 
 	i = 0;
-	if (n <= 0)
-		return (0);
-	while (s1[i] == s2[i] && (s1[i] != '\0' || s2[i] != '\0') && i < n - 1)
+	while (s1[i] == s2[i] && (s1[i] != '\0' || s2[i] != '\0'))
 		i++;
 	return ((unsigned char const)s1[i] - (unsigned char const)s2[i]);
 }
@@ -105,27 +103,57 @@ void create_tokens(t_data *data)
         }
         while(data->input[i] && ((data->input[i] == ' ' || data->input[i] == '\t')))
             i++;
+        if(data->input[i - 1] == ' ')
+        {
+            j = i - 1;
+            fill_tokens(data, i, j, NONE);
+        }
         j = i;
         while(data->input[i] && ((!(data->input[i] == ' ' || data->input[i] == '\t')) && (!(data->input[i] == '"' || data->input[i] == '\''))))
             i++;
         fill_tokens(data, i, j, NONE);
     }
-    // while(data->tokens != NULL)
-    // {
-    //     printf("original token is --> |%s| \n", data->tokens->original_content);
-    //     printf("status is %d\n", data->tokens->quotes);
-    //     data->tokens = data->tokens->next;
-    // }
+    while(data->tokens != NULL)
+    {
+        printf("original token is --> |%s| \n", data->tokens->original_content);
+        printf("status is %d\n", data->tokens->quotes);
+        data->tokens = data->tokens->next;
+    }
 }
 
-int is_command(t_data *data)
+void is_command(char *content, t_token *token)
 {
+    if(ft_strcmp("env", content) || ft_strcmp("cd", content) || ft_strcmp("pwd", content))
+    {
+        token->type = COMMAND;
+        //return(1);
+    }
+    else if(ft_strcmp("export", content) || ft_strcmp("unset", content) || ft_strcmp("exit", content))
+    {
+        token->type = COMMAND;
+        //return(1);
+    }
+    //return(0);
 }
-int is_env(t_data *data)
+int is_env(char *content)
 {
 }
 void sorting(t_data *data)
 {
+    int i;
+
+    i = 0;
+    while(data->tokens != NULL)
+    {
+        if(is_command(data->tokens->original_content))
+            data->tokens = data->tokens->next;
+        while(data->tokens && data->tokens->original_content[i])
+        {
+            if(data->tokens->original_content[i] == '$' && i = 0)
+                token->type = ENV;
+            
+        }
+    }
 }
 
 void start_shell(t_data *data)
@@ -135,7 +163,9 @@ void start_shell(t_data *data)
         data->input = readline("Verishen: ");
         create_tokens(data);
         //free_data(data);
-        sorting(data);
+        // if(!sorting(data))
+        //     exit(2);
+        // checking(data);
         //system("leaks minishell");
     }
 }
